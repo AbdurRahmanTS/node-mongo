f22ee0f11da1fd9c8113655994c27ef494a494c4
Abdur Rahman <abdurrahmants@gmail.com>
2020-09-27 11:47:02 +0600

    node js basic completed

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const users = ['Asad', 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
+
+// get
 app.get('/', (req, res) => {
-    res.send('I konw how to open node...YAY')
+    const fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit)
+});
+
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+});
+
+// post users
+app.post('/adduser', (req, res) => {
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 })
+
 
 
 
